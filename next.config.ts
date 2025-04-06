924020b3edc89053460ebca5a8b7e5ee0b00f6af
Brainard Setiawan <146184361+brobrenat@users.noreply.github.com>
2025-04-06 23:24:10 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com"], // ✅ add the image domain you're using
+    unoptimized: true, // ✅ add the image domain you're using
   },
 };
 
